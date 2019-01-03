a5b276259d5ab7c3bac3d9ae3c66924fea0ec132
Lars <larsmackenzie@gmail.com>
2019-01-03 13:02:01 -0600

    multitypes

--- main.js
+++ main.js
@@ -10,6 +10,55 @@ var y = 20;
 var isBeginner = true;
 var total = 1;
 var name = 'Lars';
+// VSCode will only recommend methods that correspond with each type -- e.g. concat for strings
+var fullName = name.concat(' Mackenzie');
 // multi-line variable with embedded values. must use backticks. line break displays in console.
-var sentence = "My name is " + name + ".\nI have written " + total + " program(s) in TypeScript. \nI am a beginner: " + isBeginner;
+var sentence = "My name is " + fullName + ".\nI have written " + total + " program(s) in TypeScript. \nI am a beginner: " + isBeginner;
 console.log(sentence);
+var n = null;
+var u = undefined;
+// null & undefined are subtypes of all types 
+var isNew = null;
+var myName = undefined;
+// both declare an array of numbers. either syntax is fine
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+// tuple -- allows for two types within one array 
+// order of values has to match order and quantity of types.
+var person1 = ['Chris', 22];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
+// any type can be reassigned -- good to use if you're relying upon user input 
+var randomValue = 10;
+randomValue = true;
+randomValue = 'hi';
+// this function checks to see if an object has a name and returns the name if it does 
+function hasName(obj) {
+    return !!obj &&
+        typeof obj === "object" &&
+        "name" in obj;
+}
+var myVariable = 10;
+myVariable = { name: 'Reed' };
+myVariable = { age: 8 };
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+// type inference 
+// if a variable is declared without an initialized value, it can be redeclared to new types without problem. 
+var a;
+a = 10;
+a = true;
+// if a variable is declared with an intialized value but no type, its type is inferred and the type cannot be reassigned.
+var b = 20;
+// b = true; type error 
+var multiType;
+multiType = 20;
+multiType = true;
--- main.ts
+++ main.ts
@@ -13,9 +13,64 @@ const y = 20;
 let isBeginner: boolean = true; 
 let total: number = 1; 
 let name: string = 'Lars'; 
+// VSCode will only recommend methods that correspond with each type -- e.g. concat for strings
+let fullName: string = name.concat(' Mackenzie'); 
 
 // multi-line variable with embedded values. must use backticks. line break displays in console.
-let sentence: string = `My name is ${name}.
+let sentence: string = `My name is ${fullName}.
 I have written ${total} program(s) in TypeScript. 
 I am a beginner: ${isBeginner}`; 
 console.log(sentence);
+
+
+let n: null = null;
+let u: undefined = undefined; 
+
+// null & undefined are subtypes of all types 
+let isNew: boolean = null;
+let myName: string = undefined; 
+
+// both declare an array of numbers. either syntax is fine
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+// tuple -- allows for two types within one array 
+// order of values has to match order and quantity of types.
+let person1: [string, number] = ['Chris', 22]; 
+
+enum Color {Red, Green, Blue}; 
+let c: Color = Color.Green; 
+console.log(c);
+
+// any type can be reassigned -- good to use if you're relying upon user input 
+let randomValue: any = 10; 
+randomValue = true; 
+randomValue = 'hi'; 
+
+// this function checks to see if an object has a name and returns the name if it does 
+function hasName(obj: any): obj is { name: string } { 
+    return !!obj && 
+        typeof obj === "object" && 
+        "name" in obj
+}
+
+let myVariable: unknown = 10; 
+myVariable = {name: 'Reed'}
+myVariable = {age: 8}
+if (hasName(myVariable)){
+    console.log(myVariable.name); 
+}
+
+// type inference 
+// if a variable is declared without an initialized value, it can be redeclared to new types without problem. 
+let a; 
+a = 10; 
+a = true; 
+
+// if a variable is declared with an intialized value but no type, its type is inferred and the type cannot be reassigned.
+let b = 20; 
+// b = true; type error 
+
+let multiType: number | boolean; 
+multiType = 20; 
+multiType = true; 
